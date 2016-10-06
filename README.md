5efcb4b3dfc5aec6cbce79c3c5c54bd4458120a2
jdhancock88 <jdhancock@gmail.com>
2016-10-06 15:53:53 -0500

    Bundles javascript file for npm use. Makes some small changes to file names for interactive template use.

--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 
 It's a custom loading animation that can be used to cover your page while it loads or while data is being loaded in from somewhere else. It's really up to you.
 
-## How to use it
+## Using the script
 
-Include the the two images (_dmn_d.svg, _dmn_polygon.png), javascript, and style sheet, then call the `dnLoader` function on the `body` element. The `dnLoader` function accepts two arguments: `display` and `remove`.
+### With NPM
 
-For example:
+```
+npm install dnpolyloader
+```
+
+```javascript
+var $ = require('jquery');
+require('dnpolyloader');
+```
+
+### To use with interactives template
+
+Place in your build/images folder:_dmn_d.svg, _dmn_polygon.png
+Place in your build/js folder: +loader.js
+Place in your build/sass folder: +loader.scss
+
+Then call the `dnLoader` function on the `body` element. The `dnLoader` function accepts two arguments: `display` and `remove`.
 
 ```javascript
 // do something that requires a loading animation
@@ -15,6 +30,6 @@ $(".get-data-button").click(function() {
     $.getJSON("data.js").done(function(data) {
         $("body").dnLoader("remove");
         console.log(data);
-    })
-})
+    });
+});
 ```
--- js/+loader.js
+++ js/+loader.js
@@ -1,5 +1,32 @@
-(function($) {
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
 
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     $.fn.dnLoader = function(action) {
 
         var self = this;
@@ -26,5 +53,4 @@
             self.removeClass("no-scroll").css("position", bodyPosition).find($(".dn-loader-block")).remove();
         }
     };
-
-}(jQuery));
+}));
